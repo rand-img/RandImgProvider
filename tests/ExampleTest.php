1ae691879e8ba0a9678839a850064e885ca3c3be
Siro Diaz <sirodiaz93@gmail.com>
2018-07-18 16:29:59 +0200

    Initial commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Siro\Tests;
+
+class ExampleTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Test that true does in fact equal true
+     */
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
